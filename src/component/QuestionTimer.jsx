591f5f2515d22780c86937f44da7a4b0509f0738
Magalmund <nikitaandrjeev@gmail.com>
2024-08-02 23:26:18 +0300

    updates

--- src/component/QuestionTimer.jsx
+++ src/component/QuestionTimer.jsx
@@ -5,7 +5,7 @@ const QuestionTimer = ({timeout, onTimeout, mode}) => {
     const [remainingTime, setRemainingTime] = useState(timeout);
 
     useEffect(() => {
-        console.log('SETTING TIMEOUT')
+        // console.log('SETTING TIMEOUT');
         const timer = setTimeout(onTimeout, timeout);
 
         return () => clearTimeout(timer);
@@ -13,7 +13,7 @@ const QuestionTimer = ({timeout, onTimeout, mode}) => {
     }, [timeout, onTimeout]);
 
     useEffect(() => {
-        console.log('SETTING INTERVAL')
+        // console.log('SETTING INTERVAL');
         const interval = setInterval(() => {
             setRemainingTime(prevState => prevState - 100);
         }, 100)
